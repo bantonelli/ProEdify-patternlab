6ad6dbdea2fc0376f57c22457d3ed74992549fbd
Brandon Antonelli <bantonelli07@gmail.com>
2017-03-10 18:54:16 -0500

    Add Babel compilation for JS using ES2105 preset

--- source/js/src/test.js
+++ source/js/src/test.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+let blegh = 10;
+
+class Person {
+    constructor (name) {
+        this.name = name;        
+    }
+}
+
+const person1 = new Person("WHOA");
+console.log([blegh, person1]);
