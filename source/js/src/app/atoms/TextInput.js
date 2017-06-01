324a92486fadfa58a87d9a1ccca720d4ba1d53fe
Brandon Antonelli <bantonelli07@gmail.com>
2017-05-31 23:06:15 -0400

    Convert text-input to VueJS component - Put component logic into JS module - Put data for text-input pattern in patternlab.

--- source/js/src/app/atoms/TextInput.js
+++ source/js/src/app/atoms/TextInput.js
@@ -1,11 +1,3 @@
-const radioTemplate = `
-<div class="radio" :class="classes">
-    <input class="radio__input" type="radio" :id="id" :value="formvalue" @change="changed" :name="formname">
-    <div class="radio__fill"></div>
-    <label class="radio__label" :for="id">\{{text}}</label>
-</div>
-`;
-
 const textInputTemplate = `
 <div class="text-input" :class="classes">
     <input type="text" class="text-input__input" @keyup="changed" :placeholder="placeholder" pattern=".{2,}" required>
@@ -14,13 +6,13 @@ const textInputTemplate = `
 `;
 
 export default {
-    template: radioTemplate,
+    template: textInputTemplate,
     // name: 'checkbox-component',
-    props: ['placeholder', 'classes', 'formvalue'],
+    props: ['placeholder', 'classes'],
     methods: {
         changed: function (event) {
             var value = event.target.value;
-            console.log("Value: ", value)
+            console.log("Value: ", value);
             this.$emit('input', value);    
         }
     }
